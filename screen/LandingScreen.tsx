3f0a8dd0e6d21d04e0cab759eafa0919a63954f7
Robert Wan <wantunglok@gmail.com>
2024-07-20 21:31:45 +1000

    test api call using axios

--- screen/LandingScreen.tsx
+++ screen/LandingScreen.tsx
@@ -1,37 +1,99 @@
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {FC, useState} from 'react';
-import {Button, View} from 'react-native';
+import {FC, useEffect, useReducer, useState} from 'react';
+import {Button, Text, View} from 'react-native';
 import {RootStackParamList} from '../App';
 import MyTextField from '../component/MyTextField';
 import MyButton from '../component/MyButton';
+import axios from 'axios';
+
+interface Student {
+  name: string;
+  course: string;
+}
+
+interface a {
+  data: Student[] | null;
+  errorMessage: string | null;
+  downloading: boolean;
+}
+
+const initialState: a = {
+  data: null,
+  errorMessage: null,
+  downloading: false,
+};
+
+interface Action {
+  type: 'DOWNLOAD' | 'SUCCESS' | 'ERROR';
+  payload?: any;
+}
+
+const reducer = (state: a, action: Action) => {
+  switch (action.type) {
+    case 'DOWNLOAD':
+      return {
+        ...state,
+        data: null,
+        errorMessage: null,
+        downloading: true,
+      };
+    case 'SUCCESS':
+      return {
+        ...state,
+        data: action.payload,
+        error: null,
+        downloading: false,
+      };
+    case 'ERROR':
+      return {
+        ...state,
+        data: null,
+        error: action.payload,
+        downloading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+const download = (dispatch: React.Dispatch<Action>) => {
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ3YW5sb2siLCJleHAiOjE3MjE0NzM1Nzl9.XIXv-myWHjLifZXuUqhWs2a7YlPbNKYgsyU3JT0tVuY';
+  dispatch({type: 'DOWNLOAD'});
+  const serverAddress = 'http://10.0.2.2:8000/student/';
+  console.log(serverAddress);
+  axios
+    .get(serverAddress, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(response => {
+      console.log(response.data);
+      dispatch({type: 'SUCCESS', payload: response.data});
+    })
+    .catch(error => {
+      dispatch({type: 'ERROR', payload: error.message});
+    });
+};
 
 const LandingScreen: FC<{
   navigation: NativeStackNavigationProp<RootStackParamList, 'LandingScreen'>;
 }> = ({navigation}) => {
-  const [estimatedPropertyValue, setEstimatedPropertyValue] = useState('');
-  const [borrowAmount, setBorrowAmount] = useState('');
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const {data, errorMessage, downloading} = state;
   return (
     <View style={{flex: 1}}>
-      <MyTextField
-        title="Estimated property value"
-        value={estimatedPropertyValue}
-        setValue={setEstimatedPropertyValue}
-        marginTop={16}
-      />
-      <MyTextField
-        title="Borrow amount"
-        value={borrowAmount}
-        setValue={setBorrowAmount}
-        marginTop={16}
-      />
-      <MyButton
-        title="CALCULATE"
-        onPress={() =>
-          navigation.navigate('SecondScreen', {
-            data: estimatedPropertyValue + ' ::: ' + borrowAmount,
-          })
-        }
-      />
+      <View style={{backgroundColor: 'blue'}}>
+        {downloading && <Text>Downloading</Text>}
+        {!downloading && errorMessage && <Text>{errorMessage}</Text>}
+        {!downloading &&
+          data &&
+          data.map((student: Student, index: number) => (
+            <Text key={index}>{student.name}</Text>
+          ))}
+        <Button title="Button" onPress={() => download(dispatch)} />
+      </View>
     </View>
   );
 };
--- screen/SecondScreen.tsx
+++ screen/SecondScreen.tsx
@@ -6,10 +6,12 @@ import {RootStackParamList} from '../App';
 const SecondScreen: FC<{
   route: RouteProp<RootStackParamList, 'SecondScreen'>;
 }> = ({route}) => {
+  var data = route.params != null && route.params.data == null ? route.params.data : "Hello";
+  
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Screen Two</Text>
-      <Text>{route.params.data}</Text>
+      <Text>{data}</Text>
     </View>
   );
 };
